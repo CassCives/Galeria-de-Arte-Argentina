5e21effa5f71b8470722633dfdf48eae7947e9e8
Cassius Cives Muñoz <cass-cives@hotmail.com>
2024-06-05 12:41:02 -0300

    se agrego un cubo, un plano y controles, falta texturas}

--- js/main.js
+++ js/main.js
@@ -15,12 +15,55 @@ document.body.appendChild(renderer.domElement); //añade renderer a html
 
 //luz de ambiente
 let ambientLight = new THREE.AmbientLight(0x101010,1.0); //color, intensidad
-ambientLight.position = camera.position; //la luz sigue a la camara
+//ambientLight.position = camera.position; //la luz sigue a la camara
 scene.add(ambientLight);
 //luz direccional
 let sunLight = new THREE.DirectionalLight(0xddddd,1.0);
 sunLight.position.y = 15;
 scene.add(sunLight);
 
-//renderizar
-renderer.render(scene,camera);
+let geometry = new THREE.BoxGeometry(1, 1, 1); //boxgeometry es la forma del objeto
+let material = new THREE.MeshBasicMaterial({color: 0xff000}); //color del objeto
+let cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+//crear el plano del piso
+//texturas del piso
+let textureLoader = new THREE.TextureLoader(); 
+textureLoader.load("img/WoodFloor.jpg");
+let planeGeometry = new THREE.PlaneGeometry(50,50);//ancho y alto
+let planeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide});
+let floorPlane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+scene.add(floorPlane);
+
+//controles
+document.addEventListener("keydown", onKeyDown, false);
+
+function onKeyDown(event){
+    //se usan keycodes para saber que tecla es
+    let keycode = event.which;
+    if(keycode == 39){
+        camera.translateX(-0.05);
+    }
+    else if(keycode == 37){
+        camera.translateX(0.05);
+    }
+    else if(keycode == 38){
+        camera.translateY(-0.05);
+    }
+    else if(keycode == 40){
+        camera.translateY(0.05);
+    }
+};
+
+let render = function() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene,camera);//renderizar
+
+    requestAnimationFrame(render);//1 vez por frame, independiente de framerate
+};
+
+render();
