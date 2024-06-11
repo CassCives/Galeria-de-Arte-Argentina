18bce5853e09f38eee30dacbc563a91f82e00270
casscives <casscives@gmail.com>
2024-06-11 15:06:03 +0000

    nueva libreria

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import { PointerLockControls } from 'three-stdlib';
 const scene = new THREE.Scene(); //crear la escena
 
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000); //camara invisible, es un pov, da profundidad, parametros fov y aspect ratio, lejos y cerca
