e85363bac751a9027d0a2334ff1266a42fc7738c
Cassius Cives <cass-cives@hotmail.com>
2024-06-30 02:01:48 -0300

    borrado comentarios

--- main.js
+++ main.js
@@ -32,18 +32,6 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 //crear el plano del piso
-//texturas del piso
-// const floorTexture = new THREE.TextureLoader().load("img/WoodFloor.jpg");
-// floorTexture.wrapS = THREE.RepeatWrapping;
-// floorTexture.wrapT = THREE.RepeatWrapping;
-// floorTexture.repeat.set(20,20);
-// const planeGeometry = new THREE.PlaneGeometry(50,50);//ancho y alto
-// const planeMaterial = new THREE.MeshBasicMaterial({map: floorTexture, side: THREE.DoubleSide});
-// const floorPlane = new THREE.Mesh(planeGeometry, planeMaterial);
-
-// floorPlane.rotation.x = Math.PI/2; //rotacion de 90 grados
-// floorPlane.position.y = -Math.PI;
-// scene.add(floorPlane);
 setupFloor(scene);
 
 //crear paredes
@@ -84,19 +72,9 @@ const backWall = new THREE.Mesh(
 backWall.position.z = 25;
 wallGroup.add(backWall);
 
-// for(let i=0;i<wallGroup.children.length;i++){
-//     wallGroup.children[i].BoundingBox = new THREE.Box3();
-//     wallGroup.children[i].BoundingBox.setFromObject(wallGroup.children[i]);
-// }
 createBoundingBoxes(wallGroup)
 
 //crear techo
-// const ceilingGeometry = new THREE.PlaneGeometry(50,50);
-// const ceilingMaterial = new THREE.MeshBasicMaterial(({color: "white"}))
-// const ceilingPlane = new THREE.Mesh(ceilingGeometry,ceilingMaterial);
-// ceilingPlane.rotation.x = Math.PI/2;
-// ceilingPlane.position.y = 10;
-// scene.add(ceilingPlane);
 const textureLoader = new THREE.TextureLoader();
 createCeiling(scene, textureLoader);
 
